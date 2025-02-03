9a49db5b1a0749aa064eff798719dbc1a88a6def
Daniel Raniz Raneland <raniz@factor10.com>
2025-02-03 13:52:24 +0100

    Add example source code

---
+++ Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using Example.Json;
+using Example.Json.User;
+
+namespace Example;
+
+public class Program
+{
+    [JsonExample("User")]
+    private static string UserJson = @"
+    {
+        ""id"": 1,
+        ""name"": ""Jane Doe"",
+        ""email"": ""jane.doe@acme.com"",
+        ""isVerified"": true,
+        ""createdAt"": ""2024-09-05T12:34:56Z""
+    }";
+
+    public static void Main(string[] args)
+    {
+        var user = new User
+        {
+            Id = 2,
+            Name = "John Doe",
+            Email = "john.doe@acme.com",
+            IsVerified = false,
+            CreatedAt = DateTime.Now,
+        };
+
+        Console.WriteLine(user.ToString());
+    }
+}
